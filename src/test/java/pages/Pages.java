22768768f9a77488c41957f06485589e0d5a9098
bojantubic <bojantubic2411@gmail.com>
2023-01-22 20:03:43 +0100

    Promene

--- src/test/java/pages/Pages.java
+++ src/test/java/pages/Pages.java
@@ -18,14 +18,14 @@ public class Pages {
     @FindBy(id = "login-button")
     private WebElement loginButton;
 
-    @FindBy(id = "id=\"add-to-cart-sauce-labs-backpack\"")
-    private WebElement addButton;
-
-    @FindBy(xpath = "//*[@id=\"shopping_cart_container\"]/a")
-    private WebElement cart;
-
-    @FindBy(id = "id=\"remove-sauce-labs-backpack\"")
-    private WebElement removeButton;
+//    @FindBy(id = "add-to-cart-sauce-labs-backpack")
+//    private WebElement addButton;
+//
+//    @FindBy(xpath = "//*[@id=\"shopping_cart_container\"]/a")
+//    private WebElement cart;
+//
+//    @FindBy(id = "remove-sauce-labs-backpack")
+//    private WebElement removeButton;
 
 //    @FindBy(xpath = "//*[@id=\"login_button_container\"]/div/form/div[3]/h3")
 //    private WebElement errorMessage;
@@ -46,12 +46,12 @@ public class Pages {
         fillUsernamePassword(username, password);
         loginClick();
     }
-    public void setAddToCart(){
-        addButton.click();
-    }
-    public void removeFromCart(){
-        removeButton.click();
-    }
-    //public void error(String expectedMessage, String errorMessage)
+//    public void setAddToCart(){
+//        addButton.click();
+//    }
+//    public void removeFromCart(){
+//        removeButton.click();
+//    }
+//    //public void error(String expectedMessage, String errorMessage)
 
 }
